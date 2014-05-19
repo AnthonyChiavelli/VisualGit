4624d15774e4d2255667c9cd2a409b377dbc3aa4
Anthony <AnthonyReid99@gmail.com>
2014-05-19 15:29:31 -0400

    Added resource files

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt4 import QtGui, QtCore, uic
+
+class TestApp(QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+
+        # Load and display UI file
+        self.ui = uic.loadUi('/home/anthony/dev/projects/VisualGit/ui/mainwindow.ui')
+        self.ui.show()
+
+        # Connect testButton's clicked() signal to our testFunc() function
+        self.connect(self.ui.testButton, QtCore.SIGNAL("clicked()"), testFunc)
+
+
+def testFunc():
+    win.ui.commitMessageTextEdit.setText('Message!')
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    win = TestApp()
+    sys.exit(app.exec_())
