b2155e6c4d05692385eb062da3461403084fe34d
Anthony <AnthonyReid99@gmail.com>
2014-05-28 13:47:00 +0200

    Create two loggers
    Create a logger for logging program execution infomation to
    stdout/err, and another for logging all git interactions to
    a file. Implement custom formatting for the logger.

---
+++ src/logger/__init__.py
@@ -0,0 +1,8 @@
+"""
+Provides basic logging capability through the logging module
+
+The logging module provides low level logging capabilities that can be
+adapted to various output channels and present in a variety of formats.
+This package wraps that module to provide logging facilities that are
+useful to this program and simple to use
+"""
