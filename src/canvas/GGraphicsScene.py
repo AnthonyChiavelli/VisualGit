8a3689333fa0550d6088fe0679369b85c63de35c
Anthony Reid <AnthonyReid99@gmail.com>
2014-06-02 17:12:37 +0200

    Implement conversion from commit to commit node
    Create function to recursively parse a commit tree and convert it into
    a commit node tree of the same structure. This newly created tree can
    then be fed to a tree drawing algorithm to measure the position of its
    nodes, then rendered to the canvas. Not tested

--- src/canvas/GGraphicsScene.py
+++ src/canvas/GGraphicsScene.py
@@ -28,3 +28,88 @@ class GGraphicsScene(QtGui.QGraphicsScene):
         super().__init__()
 
         self.setBackgroundBrush(QBrush(CANVAS_BACKGROUND_COLOR))
+
+        # A mapping of sha to GCommitNode to avoid redrawing the same
+        # node twice (as it may be a child of multiple parents)
+        self.sha_to_node = {}
+
+
+    def render_scene(self, commit):
+        """
+        Renders the various elements of the canvas
+
+        First, the commits are parsed recursively and rendered onto
+        the canvas as GCommitNodes in a tree arrangement. Arrows
+        are drawn to show parent-child relationships.
+
+        Then, branch and tag labels are drawn next to their commitss
+        """
+
+        # Convert our Commit tree to a tree of GCommitNode objects
+
+
+        # Generate graphical tree from root commit using chosen
+        # algorithm
+        # TODO
+        self._render_commits(None)
+
+        # Render labels
+        # TODO
+
+    def _render_commits(self, g_commit_node):
+        """
+        Render a graph of commits onto the canvas
+
+        GCommitNodes are the graphical objects that represent and
+        encapsulate commit nodes. Previously our tree of Commit objects
+        has been traversed and each node converted to a GCommitNode and
+        assigned coordinates based on the particular tree drawing
+        algorithm chosen. This method simple renders each node at its
+        given x and y coordinates in a preorder traversal.
+        """
+
+        # Add this node to the scene
+        self.addItem(g_commit_node)
+
+        # Recursively call on its children to be added to scene
+        map(self._render_commits, g_commit_node.children)
+
+    def node_tree_from_commit(self, commit, parent = None):
+        """
+        Converts a Commit tree into a GCommitNode tree
+
+        Commit objects represent commits made in a repository. They are
+        represented by, and aggregated by, GCommitNode objects, which
+        are graphics objects that can be drawn on the canvas.
+
+        This method traverses a tree rooted at the provided Commit and
+        constructs a GCommitNode tree of the same structure, preserving
+        parent-child relationships.
+        """
+
+        # Make sure we haven't already processed this commit (via
+        # another parent)
+        if commit.sha in self.sha_to_node:
+            g_commit_node = self.sha_to_node[commit.sha]
+        # Otherwise we need a new one
+        else:
+            g_commit_node = GCommitNode(commit)
+
+        # If this is a recursive call, attach calling parent
+        if parent:
+            g_commit_node.parents.append(parent)
+
+        # If our commit has children, recursively call ourselves
+        # on each child and add the gcommitnode returned to our
+        # children set
+        if commit.children:
+            for commit_child in commit.children:
+                new_g_commit_node = self.node_tree_from_commit(commit_child, g_commit_node)
+                g_commit_node.children.append(new_g_commit_node)
+            # TODO handle arrows
+
+        # Add this to our global mapping of shas to gcommitnodes
+        self.sha_to_node[commit.sha] = g_commit_node
+
+        # Return newly converted gcommitnode
+        return g_commit_node
---
+++ src/canvas/rendering_algorithms.py
@@ -0,0 +1,76 @@
+"""
+A collection of algorithms for drawing a tree/graph
+
+These generic algorithms traverse a tree and assign an x and y
+coordinates to each node in the tree. Drawing a tree is an np-complete
+problem, and drawing an attractive tree is a human problem; both are
+unsolvable. Various tree drawing algorithms exist, each with guiding
+principles, and problems they attempt to solve.
+
+Some common principles of tree drawing include:
+1. Connecting lines should not cross
+2. Nodes within the same depth should be drawn in line with each other
+3. Parents should be centered above their children
+4. The tree should not be wider than necessary
+
+Git repositories are not actually trees, but acyclic directed graphs.
+As such, we may often have to forgo some of the principles listed
+above, and we must apply modifications to the algorithms used. However,
+because git repos are usually mostly tree-like, and many people prefer
+to visualize them in a hierarchy, we still opt to use tree-drawing
+strategies and make the necessary corrections.
+"""
+from canvas import GCommitNode
+
+
+def minimum_width(root_commit):
+    """
+    Draw a minimally wide tree
+
+    This algorithm produces a tree of minimum width, with each
+    generation on a different row. The tree begins at the top and grows
+    downward. Though for general purposes the trees drawn by this are
+    considered somewhat ugly, their narrow, linear shape is decently
+    well-suited for git repos.
+
+    In particular, children of a node are drawn the next level down,
+    at the left-most available x position. The results in a compact,
+    linear tree, but lineage can be difficult to follow.
+
+    :param root_commit: the root commit of the tree to be rendered
+    """
+    _minimum_width(root_commit)
+
+
+def _minimum_width(commit, next_x_slots=[0], depth=0):
+    """
+    Helper function for minimum_width()
+
+    This helper function performs the actual rendering.
+
+    :param commit: The current commit being processed
+    :param next_x_slots: Keeps track of the next available x position
+        for each row
+    :param depth: the depth of the current commit being processed
+    """
+
+    # Wrap this Commit in a GCommitNode
+    g_commit_node = GCommitNode(commit)
+
+    # Position based on depth (y) and left-most available column (x)
+    x_spacing, y_spacing = 100, 100  # TODO accept as setting parameter
+    x_pos = next_x_slots[depth] * x_spacing
+    y_pos = depth * y_spacing
+    g_commit_node.setPos(x_pos, y_pos)
+
+    # Column at this depth was used, move on to next one
+    next_x_slots[depth] += 1
+
+    for child in commit.children:
+
+
+
+
+
+
+
